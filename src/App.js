bf1b97dbf2c8d83b97ecdc2aeb2dc0c7eb566a4b
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-01-20 18:41:56 +0545

    add carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/header_footer/Header';
+import Featured from './components/featured';
 import './resources/styles.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App" style={{backgroundColor: 'lightblue', height: '1000px'}}>
         <Header />
+        <Featured />
       </div>
     );
   }
---
+++ src/components/featured/Carrousel.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Slider from "react-slick";
+import slideone from "../../resources/images/slide_one.jpg";
+import slidetwo from "../../resources/images/slide_two.jpg";
+import slidethree from "../../resources/images/slide_three.jpg";
+
+class Carrousel extends React.Component {
+  render() {
+    const settings = {
+      dot: false,
+      infinite: true,
+      speed: 500,
+      autoplay: true
+    };
+
+    return (
+      <div
+        className="carrousel_wrapper"
+        style={{
+          overflow: "hidden",
+          height: '100vh'
+        }}
+      >
+        <Slider {...settings}>
+          <div>
+            <div className="carrousel_image"
+              style={{
+                background: `url(${slideone})`,
+                 height: '100vh'
+
+              }}
+            />
+          </div>
+          <div>
+            <div className="carrousel_image"
+              style={{
+                background: `url(${slidetwo})`,
+                 height: '100vh'
+
+              }}
+            />
+          </div>
+          <div>
+            <div className="carrousel_image"
+              style={{
+                background: `url(${slidethree})`,
+                 height: '100vh'
+
+              }}
+            />
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
+
+export default Carrousel;
